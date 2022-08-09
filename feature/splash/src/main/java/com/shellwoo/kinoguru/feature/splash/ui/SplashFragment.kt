965754ba863ca6ddbf32cfd2bc699dcf9aa73c97
gukovmi <gukovmi96@yandex.ru>
2022-08-09 19:08:29 +0700

    Create splash screen

---
+++ feature/splash/src/main/java/com/shellwoo/kinoguru/feature/splash/ui/SplashFragment.kt
@@ -0,0 +1,6 @@
+package com.shellwoo.kinoguru.feature.splash.ui
+
+import androidx.fragment.app.Fragment
+import com.shellwoo.kinoguru.feature.splash.R
+
+class SplashFragment : Fragment(R.layout.splash_fragment)
