86977f8a1e6ab3c9893c07f5aae4fa81e20f3616
gukovmi <gukovmi96@yandex.ru>
2022-08-09 19:09:17 +0700

    Open splash screen

--- app/src/main/java/com/shellwoo/kinoguru/MainActivity.kt
+++ app/src/main/java/com/shellwoo/kinoguru/MainActivity.kt
@@ -2,11 +2,23 @@ package com.shellwoo.kinoguru
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.shellwoo.kinoguru.feature.splash.ui.SplashFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            openSplashFragment()
+        }
+    }
+
+    private fun openSplashFragment() {
+        supportFragmentManager.beginTransaction()
+            .add(R.id.container, SplashFragment())
+            .commit()
     }
 }
