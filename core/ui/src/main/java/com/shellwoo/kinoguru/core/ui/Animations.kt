56e7da36a5c6446729fac2dfe0cb0c25946b6e6b
gukovmi <gukovmi96@yandex.ru>
2022-08-11 21:59:16 +0700

    Create infinity view animation for cycle of increase and decrease

---
+++ core/ui/src/main/java/com/shellwoo/kinoguru/core/ui/Animations.kt
@@ -0,0 +1,27 @@
+package com.shellwoo.kinoguru.core.ui
+
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
+import android.view.View
+
+fun View.animateOfIncreaseAndDecrease(
+    minRelativeSize: Float,
+    maxRelativeSize: Float,
+    cycleDuration: Long,
+) {
+    ObjectAnimator.ofFloat(this, "scaleX", minRelativeSize, maxRelativeSize)
+        .setDuration(cycleDuration / 2)
+        .apply {
+            repeatMode = ValueAnimator.REVERSE
+            repeatCount = ValueAnimator.INFINITE
+        }
+        .start()
+
+    ObjectAnimator.ofFloat(this, "scaleY", minRelativeSize, maxRelativeSize)
+        .setDuration(cycleDuration / 2)
+        .apply {
+            repeatMode = ValueAnimator.REVERSE
+            repeatCount = ValueAnimator.INFINITE
+        }
+        .start()
+}
