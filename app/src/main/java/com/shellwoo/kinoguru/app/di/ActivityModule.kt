cefeaaa3ec8a440d42b023d244939318000b2d79
gukovmi <gukovmi96@yandex.ru>
2022-08-14 10:13:41 +0700

    Remove redundant inject BaseFragment

--- app/src/main/java/com/shellwoo/kinoguru/app/di/ActivityModule.kt
+++ app/src/main/java/com/shellwoo/kinoguru/app/di/ActivityModule.kt
@@ -2,11 +2,10 @@ package com.shellwoo.kinoguru.app.di
 
 import com.shellwoo.kinoguru.app.ui.MainActivity
 import com.shellwoo.kinoguru.core.di.ActivityScope
-import com.shellwoo.kinoguru.feature.splash.di.SplashFragmentModule
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
-@Module(includes = [SplashFragmentModule::class])
+@Module(includes = [FragmentModule::class])
 interface ActivityModule {
 
     @ActivityScope
--- app/src/main/java/com/shellwoo/kinoguru/app/di/FragmentModule.kt
+++ app/src/main/java/com/shellwoo/kinoguru/app/di/FragmentModule.kt
@@ -1,12 +1,10 @@
 package com.shellwoo.kinoguru.app.di
 
-import com.shellwoo.kinoguru.core.ui.component.BaseFragmentModule
 import com.shellwoo.kinoguru.feature.splash.di.SplashFragmentModule
 import dagger.Module
 
 @Module(
     includes = [
-        BaseFragmentModule::class,
         SplashFragmentModule::class,
     ]
 )
