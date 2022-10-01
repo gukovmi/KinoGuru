82800b75d24471e836195a41c3c2f7e33198c535
gukovmi <gukovmi96@yandex.ru>
2022-10-01 19:02:45 +0600

    Create common fragment extension for showing retry/cancel error dialog

--- core/ui/src/main/java/com/shellwoo/kinoguru/core/ui/FragmentExt.kt
+++ core/ui/src/main/java/com/shellwoo/kinoguru/core/ui/FragmentExt.kt
@@ -1,8 +1,24 @@
 package com.shellwoo.kinoguru.core.ui
 
+import android.app.AlertDialog
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 
 fun Fragment.showToast(text: String) {
     Toast.makeText(requireContext(), text, Toast.LENGTH_LONG).show()
+}
+
+fun Fragment.showRetryCancelErrorDialog(
+    onRetryAction: (() -> Unit)? = null,
+    onCancelAction: (() -> Unit)? = null,
+) {
+    AlertDialog.Builder(requireContext())
+        .setPositiveButton(R.string.error_dialog_button_retry) { _, _ -> onRetryAction?.invoke() }
+        .setNegativeButton(R.string.error_dialog_button_cancel) { _, _ -> onCancelAction?.invoke() }
+        .setOnDismissListener { onCancelAction?.invoke() }
+        .setTitle(R.string.error_dialog_title_repeatable)
+        .setIcon(R.drawable.ic_baseline_error_24)
+        .setIconAttribute(android.R.attr.alertDialogIcon)
+        .create()
+        .show()
 }
