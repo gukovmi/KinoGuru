4a1af44a8b4030bf710360f5eece4244d6560393
gukovmi <gukovmi96@yandex.ru>
2022-10-15 13:43:48 +0600

    Add selecting menu item in bottom navigation bar logic

--- feature/main/src/main/java/com/shellwoo/kinoguru/feature/main/ui/MainFragment.kt
+++ feature/main/src/main/java/com/shellwoo/kinoguru/feature/main/ui/MainFragment.kt
@@ -1,6 +1,7 @@
 package com.shellwoo.kinoguru.feature.main.ui
 
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import androidx.fragment.app.viewModels
 import com.github.terrakok.cicerone.NavigatorHolder
@@ -9,6 +10,7 @@ import com.shellwoo.kinoguru.core.navigation.di.MainNavigation
 import com.shellwoo.kinoguru.core.ui.component.BaseFragment
 import com.shellwoo.kinoguru.feature.main.R
 import com.shellwoo.kinoguru.feature.main.presentation.MainViewModel
+import kotlinx.android.synthetic.main.main_fragment.*
 import javax.inject.Inject
 
 class MainFragment : BaseFragment(R.layout.main_fragment) {
@@ -27,7 +29,23 @@ class MainFragment : BaseFragment(R.layout.main_fragment) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.openProfileScreen()
+        initListeners()
+
+        bottomNavigationBar.selectedItemId = R.id.profile
+    }
+
+    private fun initListeners() {
+        bottomNavigationBar.setOnItemSelectedListener(::handleSelectedMenuItem)
+    }
+
+    private fun handleSelectedMenuItem(menuItem: MenuItem): Boolean {
+        val selectedItemNotChanged = bottomNavigationBar.selectedItemId == menuItem.itemId
+        if (selectedItemNotChanged) return false
+
+        when (menuItem.itemId) {
+            R.id.profile -> viewModel.openProfileScreen()
+        }
+        return true
     }
 
     override fun onResume() {
