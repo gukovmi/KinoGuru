287ad09000ca99bf9413e4e8e14ea7305148b745
gukovmi <gukovmi96@yandex.ru>
2022-10-15 13:39:27 +0600

    Create NavigatorFactory (factory for wrapping AppNavigator with FragmentTransactionAnimator)

---
+++ core/navigation/src/main/java/com/shellwoo/kinoguru/core/navigation/NavigatorFactory.kt
@@ -0,0 +1,30 @@
+package com.shellwoo.kinoguru.core.navigation
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.github.terrakok.cicerone.androidx.AppNavigator
+import com.github.terrakok.cicerone.androidx.FragmentScreen
+import com.shellwoo.kinoguru.core.ui.FragmentTransactionAnimator
+import javax.inject.Inject
+
+class NavigatorFactory @Inject constructor(private val fragmentTransactionAnimator: FragmentTransactionAnimator) {
+
+    fun create(
+        activity: FragmentActivity,
+        containerId: Int,
+        fragmentManager: FragmentManager = activity.supportFragmentManager,
+    ): AppNavigator =
+        object : AppNavigator(activity, containerId, fragmentManager) {
+
+            override fun setupFragmentTransaction(
+                screen: FragmentScreen,
+                fragmentTransaction: FragmentTransaction,
+                currentFragment: Fragment?,
+                nextFragment: Fragment,
+            ) {
+                fragmentTransactionAnimator.setAnimation(nextFragment, fragmentTransaction)
+            }
+        }
+}
