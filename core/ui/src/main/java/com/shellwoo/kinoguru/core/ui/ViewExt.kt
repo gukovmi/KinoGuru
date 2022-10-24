fa0eb45e88824f069fc67c00016cb3623c27a64b
gukovmi <gukovmi96@yandex.ru>
2022-10-25 00:01:42 +0600

    Add view extensions for determine view top and left on window

---
+++ core/ui/src/main/java/com/shellwoo/kinoguru/core/ui/ViewExt.kt
@@ -0,0 +1,19 @@
+package com.shellwoo.kinoguru.core.ui
+
+import android.view.View
+
+const val POINT_ARGS_NUMBER = 2
+const val X_POSITION = 0
+const val Y_POSITION = 1
+
+fun View.getTopOnWindow(): Int {
+    val point = IntArray(POINT_ARGS_NUMBER)
+    getLocationInWindow(point)
+    return point[Y_POSITION]
+}
+
+fun View.getLeftOnWindow(): Int {
+    val point = IntArray(POINT_ARGS_NUMBER)
+    getLocationInWindow(point)
+    return point[X_POSITION]
+}
