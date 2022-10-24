5d29442c42611a8c7e6cc9e8e87cb7de672761f1
gukovmi <gukovmi96@yandex.ru>
2022-10-24 21:28:41 +0600

    Create method for getting color from current theme

---
+++ core/ui/src/main/java/com/shellwoo/kinoguru/core/ui/ThemeExt.kt
@@ -0,0 +1,21 @@
+package com.shellwoo.kinoguru.core.ui
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.util.TypedValue
+import androidx.annotation.AttrRes
+import androidx.annotation.ColorInt
+
+@ColorInt
+fun getThemeColor(context: Context, @AttrRes attr: Int, @ColorInt defaultColor: Int = 0): Int {
+    val styledAttrs = getThemeStyledAttributes(context, attr)
+    val color = styledAttrs.getColor(0, defaultColor)
+    styledAttrs.recycle()
+    return color
+}
+
+private fun getThemeStyledAttributes(context: Context, @AttrRes attr: Int): TypedArray {
+    val typedValue = TypedValue()
+    context.theme.resolveAttribute(attr, typedValue, true)
+    return context.obtainStyledAttributes(typedValue.resourceId, intArrayOf(attr))
+}
