0e66470554156f62a089db367c266ad6edadf175
gukovmi <gukovmi96@yandex.ru>
2023-04-08 13:33:30 +0500

    Add showing onboarding logic on search field

--- feature/search/src/main/java/com/shellwoo/kinoguru/feature/search/presentation/SearchViewModel.kt
+++ feature/search/src/main/java/com/shellwoo/kinoguru/feature/search/presentation/SearchViewModel.kt
@@ -3,10 +3,17 @@ package com.shellwoo.kinoguru.feature.search.presentation
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.shellwoo.kinoguru.core.presentation.SingleLiveEvent
+import com.shellwoo.kinoguru.feature.search.domain.usecase.IsSearchOnboardingShowedUseCase
+import com.shellwoo.kinoguru.feature.search.domain.usecase.SetSearchOnboardingShowedUseCase
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class SearchViewModel @Inject constructor() : ViewModel() {
+class SearchViewModel @Inject constructor(
+    private val isSearchOnboardingShowedUseCase: IsSearchOnboardingShowedUseCase,
+    private val setSearchOnboardingShowedUseCase: SetSearchOnboardingShowedUseCase,
+) : ViewModel() {
 
     private val _state = MutableLiveData<SearchState>(SearchState.Initial)
     val state: LiveData<SearchState> = _state
@@ -16,6 +23,17 @@ class SearchViewModel @Inject constructor() : ViewModel() {
 
     fun start() {
         _state.value = SearchState.Content
-        _onboardingEvent(Unit)
+
+        viewModelScope.launch { showSearchOnboardingIfNeeded() }
+    }
+
+    private suspend fun showSearchOnboardingIfNeeded() {
+        isSearchOnboardingShowedUseCase().collect { searchOnboardingShowed ->
+            val searchOnboardingNeeded = !searchOnboardingShowed
+            if (searchOnboardingNeeded) {
+                _onboardingEvent(Unit)
+                setSearchOnboardingShowedUseCase()
+            }
+        }
     }
 }
--- feature/search/src/test/java/com/shellwoo/kinoguru/feature/search/presentation/SearchViewModelTest.kt
+++ feature/search/src/test/java/com/shellwoo/kinoguru/feature/search/presentation/SearchViewModelTest.kt
@@ -2,16 +2,29 @@ package com.shellwoo.kinoguru.feature.search.presentation
 
 import androidx.lifecycle.Observer
 import com.shellwoo.kinoguru.core.test.unit.InstantTaskExecutorExtension
+import com.shellwoo.kinoguru.core.test.unit.TestCoroutineExtension
+import com.shellwoo.kinoguru.feature.search.domain.usecase.IsSearchOnboardingShowedUseCase
+import com.shellwoo.kinoguru.feature.search.domain.usecase.SetSearchOnboardingShowedUseCase
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.junit.jupiter.MockitoExtension
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.never
 import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 
-@ExtendWith(MockitoExtension::class, InstantTaskExecutorExtension::class)
+@ExtendWith(MockitoExtension::class, InstantTaskExecutorExtension::class, TestCoroutineExtension::class)
 class SearchViewModelTest {
 
-    private val viewModel = SearchViewModel()
+    private val isSearchOnboardingShowedUseCase: IsSearchOnboardingShowedUseCase = mock()
+    private val setSearchOnboardingShowedUseCase: SetSearchOnboardingShowedUseCase = mock()
+
+    private val viewModel = SearchViewModel(
+        isSearchOnboardingShowedUseCase,
+        setSearchOnboardingShowedUseCase,
+    )
 
     private val stateObserver: Observer<SearchState> = mock()
     private val onboardingEventObserver: Observer<Unit> = mock()
@@ -24,7 +37,8 @@ class SearchViewModelTest {
     }
 
     @Test
-    fun `start EXPECT content state`() {
+    fun `start EXPECT content state`() = runTest {
+        whenever(isSearchOnboardingShowedUseCase()).thenReturn(flowOf(true))
         viewModel.state.observeForever(stateObserver)
 
         viewModel.start()
@@ -33,11 +47,40 @@ class SearchViewModelTest {
     }
 
     @Test
-    fun `start EXPECT onboarding event`() {
+    fun `start, search onboarding was not showed EXPECT onboarding event`() = runTest {
+        whenever(isSearchOnboardingShowedUseCase()).thenReturn(flowOf(false))
         viewModel.onboardingEvent.observeForever(onboardingEventObserver)
 
         viewModel.start()
 
         verify(onboardingEventObserver).onChanged(Unit)
+    }
+
+    @Test
+    fun `start, search onboarding was not showed EXPECT set search onboarding showed`() = runTest {
+        whenever(isSearchOnboardingShowedUseCase()).thenReturn(flowOf(false))
+
+        viewModel.start()
+
+        verify(setSearchOnboardingShowedUseCase).invoke()
+    }
+
+    @Test
+    fun `start, search onboarding was showed EXPECT onboarding event never emit`() = runTest {
+        whenever(isSearchOnboardingShowedUseCase()).thenReturn(flowOf(true))
+        viewModel.onboardingEvent.observeForever(onboardingEventObserver)
+
+        viewModel.start()
+
+        verify(onboardingEventObserver, never()).onChanged(Unit)
+    }
+
+    @Test
+    fun `start, search onboarding not showed EXPECT set search onboarding showed never call`() = runTest {
+        whenever(isSearchOnboardingShowedUseCase()).thenReturn(flowOf(true))
+
+        viewModel.start()
+
+        verify(setSearchOnboardingShowedUseCase, never()).invoke()
     }
 }
