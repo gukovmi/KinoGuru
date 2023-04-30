3ed13baa50a69d99f58ef21088450b8a0c368472
gukovmi <gukovmi96@yandex.ru>
2023-04-30 20:29:57 +0500

    Add ViewGroup extension for inflate view

---
+++ core/ui/src/main/java/com/shellwoo/kinoguru/core/ui/ViewGroupExt.kt
@@ -0,0 +1,9 @@
+package com.shellwoo.kinoguru.core.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+
+fun ViewGroup.inflate(@LayoutRes res: Int, attachToRoot: Boolean = false): View =
+    LayoutInflater.from(context).inflate(res, this, attachToRoot)
