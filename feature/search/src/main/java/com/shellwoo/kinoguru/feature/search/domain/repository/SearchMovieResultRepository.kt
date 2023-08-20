ba64d0964c336d04822e99c4ee455cd8ec9e26d3
gukovmi <gukovmi96@yandex.ru>
2023-08-20 17:06:02 +0500

    Supply current app language for searching movie request

--- feature/search/src/main/java/com/shellwoo/kinoguru/feature/search/domain/repository/SearchMovieResultRepository.kt
+++ feature/search/src/main/java/com/shellwoo/kinoguru/feature/search/domain/repository/SearchMovieResultRepository.kt
@@ -1,8 +1,9 @@
 package com.shellwoo.kinoguru.feature.search.domain.repository
 
 import com.shellwoo.kinoguru.feature.search.domain.entity.SearchMovieResult
+import com.shellwoo.kinoguru.shared.language.domain.entity.Language
 
 interface SearchMovieResultRepository {
 
-    suspend fun get(query: String, page: Int?): SearchMovieResult
+    suspend fun get(query: String, language: Language, page: Int?): SearchMovieResult
 }
--- feature/search/src/test/java/com/shellwoo/kinoguru/feature/search/data/repository/SearchMovieResultRepositoryImplTest.kt
+++ feature/search/src/test/java/com/shellwoo/kinoguru/feature/search/data/repository/SearchMovieResultRepositoryImplTest.kt
@@ -1,9 +1,11 @@
 package com.shellwoo.kinoguru.feature.search.data.repository
 
 import com.shellwoo.kinoguru.feature.search.data.converter.SearchMovieResultModelConverter
-import com.shellwoo.kinoguru.feature.search.data.datasource.SearchMovieResultDataSource
 import com.shellwoo.kinoguru.feature.search.data.model.SearchMovieResultModel
+import com.shellwoo.kinoguru.feature.search.data.network.SearchMovieApi
 import com.shellwoo.kinoguru.feature.search.domain.entity.SearchMovieResult
+import com.shellwoo.kinoguru.shared.language.data.converter.LanguageTagConverter
+import com.shellwoo.kinoguru.shared.language.domain.entity.Language
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -15,20 +17,24 @@ import org.mockito.kotlin.whenever
 @ExtendWith(MockitoExtension::class)
 internal class SearchMovieResultRepositoryImplTest {
 
-    private val dataSource: SearchMovieResultDataSource = mock()
-    private val converter: SearchMovieResultModelConverter = mock()
+    private val api: SearchMovieApi = mock()
+    private val searchMovieResultModelConverter: SearchMovieResultModelConverter = mock()
+    private val languageTagConverter: LanguageTagConverter = mock()
 
-    private val repository = SearchMovieResultRepositoryImpl(dataSource, converter)
+    private val repository = SearchMovieResultRepositoryImpl(api, searchMovieResultModelConverter, languageTagConverter)
 
     @Test
     fun `get EXPECT search movie result`() = runTest {
         val query = "Batman"
         val model: SearchMovieResultModel = mock()
         val expected: SearchMovieResult = mock()
-        whenever(converter.from(model)).thenReturn(expected)
-        whenever(dataSource.get(query, null)).thenReturn(model)
+        val language = Language.ENGLISH
+        val languageTag = "en"
+        whenever(languageTagConverter.toTag(language)).thenReturn(languageTag)
+        whenever(api.get(query, languageTag, null)).thenReturn(model)
+        whenever(searchMovieResultModelConverter.from(model)).thenReturn(expected)
 
-        val actual = repository.get(query, null)
+        val actual = repository.get(query, language, null)
 
         assertEquals(actual, expected)
     }
