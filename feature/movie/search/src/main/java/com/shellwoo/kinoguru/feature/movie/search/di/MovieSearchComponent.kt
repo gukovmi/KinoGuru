dc07ea56a50347715a6e7ace2783268c8b95c27c
gukovmi <gukovmi96@yandex.ru>
2023-09-04 18:58:43 +0500

    Use new error's logic on movie search screen

--- feature/movie/search/src/main/java/com/shellwoo/kinoguru/feature/movie/search/di/MovieSearchComponent.kt
+++ feature/movie/search/src/main/java/com/shellwoo/kinoguru/feature/movie/search/di/MovieSearchComponent.kt
@@ -1,12 +1,13 @@
 package com.shellwoo.kinoguru.feature.movie.search.di
 
 import com.shellwoo.kinoguru.feature.movie.search.ui.MovieSearchFragment
+import com.shellwoo.kinoguru.shared.error.di.ErrorComponent
 import com.shellwoo.kinoguru.shared.language.di.LanguageDataComponent
 import com.shellwoo.kinoguru.shared.movie.di.MovieDataComponent
 import dagger.Component
 
 @Component(
-    dependencies = [MovieSearchDeps::class, MovieDataComponent::class, LanguageDataComponent::class],
+    dependencies = [ErrorComponent::class, MovieSearchDeps::class, MovieDataComponent::class, LanguageDataComponent::class],
     modules = [MovieSearchPresentationModule::class]
 )
 internal interface MovieSearchComponent {
@@ -15,6 +16,7 @@ internal interface MovieSearchComponent {
 
         fun create(movieSearchDeps: MovieSearchDeps): MovieSearchComponent =
             DaggerMovieSearchComponent.builder()
+                .errorComponent(ErrorComponent.create(movieSearchDeps.context))
                 .movieSearchDeps(movieSearchDeps)
                 .movieDataComponent(MovieDataComponent.create(movieSearchDeps.context))
                 .languageDataComponent(LanguageDataComponent.create())
