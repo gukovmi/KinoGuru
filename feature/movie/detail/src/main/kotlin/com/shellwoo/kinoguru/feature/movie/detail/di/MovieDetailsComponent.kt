ff0a83e7f087ede97066e71dfcd1bb4eba95f18e
gukovmi <gukovmi96@yandex.ru>
2023-09-08 20:39:18 +0500

    Use new error's logic on movie details screen

--- feature/movie/detail/src/main/kotlin/com/shellwoo/kinoguru/feature/movie/detail/di/MovieDetailsComponent.kt
+++ feature/movie/detail/src/main/kotlin/com/shellwoo/kinoguru/feature/movie/detail/di/MovieDetailsComponent.kt
@@ -1,12 +1,13 @@
 package com.shellwoo.kinoguru.feature.movie.detail.di
 
 import com.shellwoo.kinoguru.feature.movie.detail.ui.MovieDetailsFragment
+import com.shellwoo.kinoguru.shared.error.di.ErrorComponent
 import com.shellwoo.kinoguru.shared.language.di.LanguageDataComponent
 import com.shellwoo.kinoguru.shared.movie.di.MovieDataComponent
 import dagger.Component
 
 @Component(
-    dependencies = [MovieDetailsDeps::class, LanguageDataComponent::class, MovieDataComponent::class],
+    dependencies = [ErrorComponent::class, MovieDetailsDeps::class, LanguageDataComponent::class, MovieDataComponent::class],
     modules = [MovieDetailsPresentationModule::class]
 )
 interface MovieDetailsComponent {
@@ -16,6 +17,7 @@ interface MovieDetailsComponent {
         fun create(movieDetailsDeps: MovieDetailsDeps): MovieDetailsComponent =
             DaggerMovieDetailsComponent.builder()
                 .movieDetailsDeps(movieDetailsDeps)
+                .errorComponent(ErrorComponent.create(movieDetailsDeps.context))
                 .languageDataComponent(LanguageDataComponent.create())
                 .movieDataComponent(MovieDataComponent.create(movieDetailsDeps.context))
                 .build()
--- feature/movie/detail/src/main/kotlin/com/shellwoo/kinoguru/feature/movie/detail/di/MovieDetailsViewModelFactory.kt
+++ feature/movie/detail/src/main/kotlin/com/shellwoo/kinoguru/feature/movie/detail/di/MovieDetailsViewModelFactory.kt
@@ -5,18 +5,20 @@ import androidx.lifecycle.ViewModelProvider
 import com.shellwoo.kinoguru.feature.movie.detail.domain.scenario.GetMovieDetailsScenario
 import com.shellwoo.kinoguru.feature.movie.detail.presentation.MovieDetailsRouter
 import com.shellwoo.kinoguru.feature.movie.detail.presentation.MovieDetailsViewModel
+import com.shellwoo.kinoguru.shared.error.domain.usecase.GetBaseExceptionUseCase
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 
 class MovieDetailsViewModelFactory @AssistedInject constructor(
+    private val getBaseExceptionUseCase: GetBaseExceptionUseCase,
     private val getMovieDetailsScenario: GetMovieDetailsScenario,
     private val router: MovieDetailsRouter,
     @Assisted private val movieId: Int,
 ) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return MovieDetailsViewModel(getMovieDetailsScenario, router, movieId) as T
+        return MovieDetailsViewModel(getBaseExceptionUseCase, getMovieDetailsScenario, router, movieId) as T
     }
 
     @AssistedFactory
