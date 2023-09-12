f2aa7a9be1922b83a991819b80b5b00a2f888d1d
gukovmi <gukovmi96@yandex.ru>
2023-09-12 20:58:01 +0500

    Delegate to AppComponent creation of classes from shared-theme module

--- feature/profile/src/main/java/com/shellwoo/kinoguru/feature/profile/di/ProfileDeps.kt
+++ feature/profile/src/main/java/com/shellwoo/kinoguru/feature/profile/di/ProfileDeps.kt
@@ -2,9 +2,13 @@ package com.shellwoo.kinoguru.feature.profile.di
 
 import android.content.Context
 import com.shellwoo.kinoguru.feature.profile.presentation.ProfileRouter
+import com.shellwoo.kinoguru.shared.theme.domain.usecase.GetCurrentThemeUseCase
+import com.shellwoo.kinoguru.shared.theme.domain.usecase.SetCurrentThemeUseCase
 
 interface ProfileDeps {
 
-    val context: Context
+    val getCurrentThemeUseCase: GetCurrentThemeUseCase
+    val setCurrentThemeUseCase: SetCurrentThemeUseCase
     val profileRouter: ProfileRouter
+    val context: Context
 }
--- feature/theme/src/main/kotlin/com/shellwoo/kinoguru/feature/theme/di/ThemeComponent.kt
+++ feature/theme/src/main/kotlin/com/shellwoo/kinoguru/feature/theme/di/ThemeComponent.kt
@@ -1,11 +1,10 @@
 package com.shellwoo.kinoguru.feature.theme.di
 
 import com.shellwoo.kinoguru.feature.theme.ui.ThemeFragment
-import com.shellwoo.kinoguru.shared.theme.di.ThemeDataComponent
 import dagger.Component
 
 @Component(
-    dependencies = [ThemeDataComponent::class, ThemeDeps::class],
+    dependencies = [ThemeDeps::class],
     modules = [ThemePresentationModule::class],
 )
 interface ThemeComponent {
@@ -15,7 +14,6 @@ interface ThemeComponent {
         fun create(themeDeps: ThemeDeps): ThemeComponent =
             DaggerThemeComponent.builder()
                 .themeDeps(themeDeps)
-                .themeDataComponent(ThemeDataComponent.create(themeDeps.context))
                 .build()
     }
 
