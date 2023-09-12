a73fc5723410899a160caa8925792f4a29542985
gukovmi <gukovmi96@yandex.ru>
2023-09-12 21:19:20 +0500

    Delegate to AppComponent creation of classes from shared-user module

--- shared/user/src/main/java/com/shellwoo/kinoguru/shared/user/domain/usecase/GetCurrentUserUseCase.kt
+++ shared/user/src/main/java/com/shellwoo/kinoguru/shared/user/domain/usecase/GetCurrentUserUseCase.kt
@@ -2,8 +2,9 @@ package com.shellwoo.kinoguru.shared.user.domain.usecase
 
 import com.shellwoo.kinoguru.shared.user.domain.entity.User
 import com.shellwoo.kinoguru.shared.user.domain.repository.UserRepository
+import javax.inject.Inject
 
-class GetCurrentUserUseCase(private val userRepository: UserRepository) {
+class GetCurrentUserUseCase @Inject constructor(private val userRepository: UserRepository) {
 
     operator fun invoke(): User? =
         userRepository.get()
