f4311e96e9f06334ca0b506a77ebceacea99b752
gukovmi <gukovmi96@yandex.ru>
2023-09-14 22:58:08 +0500

    Delegate to AppComponent creation of classes from shared-language module

--- feature/language/src/main/kotlin/com/shellwoo/kinoguru/feature/language/di/LanguageDeps.kt
+++ feature/language/src/main/kotlin/com/shellwoo/kinoguru/feature/language/di/LanguageDeps.kt
@@ -2,9 +2,11 @@ package com.shellwoo.kinoguru.feature.language.di
 
 import android.content.Context
 import com.shellwoo.kinoguru.feature.language.presentation.LanguageRouter
+import com.shellwoo.kinoguru.shared.language.domain.usecase.GetAllLanguagesUseCase
 
 interface LanguageDeps {
 
-    val context: Context
+    val getAllLanguagesUseCase: GetAllLanguagesUseCase
     val languageRouter: LanguageRouter
+    val context: Context
 }
--- feature/profile/src/main/java/com/shellwoo/kinoguru/feature/profile/di/ProfileDeps.kt
+++ feature/profile/src/main/java/com/shellwoo/kinoguru/feature/profile/di/ProfileDeps.kt
@@ -2,14 +2,18 @@ package com.shellwoo.kinoguru.feature.profile.di
 
 import android.content.Context
 import com.shellwoo.kinoguru.feature.profile.presentation.ProfileRouter
+import com.shellwoo.kinoguru.shared.language.domain.usecase.GetCurrentLanguageUseCase
+import com.shellwoo.kinoguru.shared.language.domain.usecase.SetCurrentLanguageUseCase
 import com.shellwoo.kinoguru.shared.theme.domain.usecase.GetCurrentThemeUseCase
 import com.shellwoo.kinoguru.shared.theme.domain.usecase.SetCurrentThemeUseCase
 import com.shellwoo.kinoguru.shared.user.domain.usecase.GetCurrentUserUseCase
 
 interface ProfileDeps {
 
+    val getCurrentLanguageUseCase: GetCurrentLanguageUseCase
     val getCurrentThemeUseCase: GetCurrentThemeUseCase
     val getCurrentUserUseCase: GetCurrentUserUseCase
+    val setCurrentLanguageUseCase: SetCurrentLanguageUseCase
     val setCurrentThemeUseCase: SetCurrentThemeUseCase
     val profileRouter: ProfileRouter
     val context: Context
