b398170f5434166915fb4cb5dade5817abce199b
gukovmi <gukovmi96@yandex.ru>
2023-09-14 22:58:08 +0500

    Delegate to AppComponent creation of classes from shared-language module

--- feature/movie/search/src/main/java/com/shellwoo/kinoguru/feature/movie/search/di/MovieSearchDeps.kt
+++ feature/movie/search/src/main/java/com/shellwoo/kinoguru/feature/movie/search/di/MovieSearchDeps.kt
@@ -4,11 +4,13 @@ import android.content.Context
 import com.shellwoo.kinoguru.feature.movie.search.presentation.MovieSearchRouter
 import com.shellwoo.kinoguru.shared.error.domain.usecase.GetBaseExceptionUseCase
 import com.shellwoo.kinoguru.shared.error.ui.BaseExceptionMessageConverter
+import com.shellwoo.kinoguru.shared.language.domain.usecase.GetCurrentLanguageUseCase
 
 interface MovieSearchDeps {
 
-    val context: Context
     val getBaseExceptionUseCase: GetBaseExceptionUseCase
+    val getCurrentLanguageUseCase: GetCurrentLanguageUseCase
     val movieSearchRouter: MovieSearchRouter
     val baseExceptionMessageConverter: BaseExceptionMessageConverter
+    val context: Context
 }
