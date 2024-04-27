e2aeacdb009821f203d551911e9fdd16ba44bb93
gukovmi <gukovmi96@yandex.ru>
2024-04-27 11:22:34 +0700

    Migrate language screen on VB

--- feature/language/src/main/kotlin/com/shellwoo/kinoguru/feature/language/ui/LanguageViewHolder.kt
+++ feature/language/src/main/kotlin/com/shellwoo/kinoguru/feature/language/ui/LanguageViewHolder.kt
@@ -2,11 +2,12 @@ package com.shellwoo.kinoguru.feature.language.ui
 
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.shellwoo.kinoguru.core.ui.ext.inflate
 import com.shellwoo.kinoguru.feature.language.R
+import com.shellwoo.kinoguru.feature.language.databinding.LanguageItemBinding
 import com.shellwoo.kinoguru.shared.language.domain.entity.Language
 import com.shellwoo.kinoguru.shared.language.ui.LanguageNameConverter
-import kotlinx.android.synthetic.main.language_item.view.*
 
 class LanguageViewHolder(
     private val languageNameConverter: LanguageNameConverter,
@@ -15,15 +16,17 @@ class LanguageViewHolder(
     parent: ViewGroup
 ) : RecyclerView.ViewHolder(parent.inflate(R.layout.language_item, false)) {
 
+    private val binding by viewBinding(LanguageItemBinding::bind)
+
     fun bind(language: Language) {
-        with(itemView) {
+        with(binding) {
             val languageName = languageNameConverter.toName(language)
             name.text = languageName
 
             val languageFlag = languageFlagConverter.toFlag(language)
             flag.setImageDrawable(languageFlag)
 
-            onClickListener?.let { itemView.setOnClickListener { it(language) } }
+            onClickListener?.let { root.setOnClickListener { it(language) } }
         }
     }
 }
