de4154a7646b01d21cd5c2f5737f2cbaeba4f1aa
gukovmi <gukovmi96@yandex.ru>
2024-05-01 00:22:25 +0700

    Migrate movie search screen on VB

--- feature/movie/search/src/main/java/com/shellwoo/kinoguru/feature/movie/search/ui/MovieSearchSuccessItemViewHolder.kt
+++ feature/movie/search/src/main/java/com/shellwoo/kinoguru/feature/movie/search/ui/MovieSearchSuccessItemViewHolder.kt
@@ -6,6 +6,7 @@ import androidx.annotation.ColorInt
 import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestManager
 import com.bumptech.glide.load.DataSource
@@ -15,16 +16,18 @@ import com.bumptech.glide.request.target.Target
 import com.shellwoo.kinoguru.core.ui.ext.getThemeColor
 import com.shellwoo.kinoguru.core.ui.ext.inflate
 import com.shellwoo.kinoguru.feature.movie.search.R
+import com.shellwoo.kinoguru.feature.movie.search.databinding.MovieSearchSuccessItemBinding
 import com.shellwoo.kinoguru.feature.movie.search.presentation.MovieSearchItem
 import com.shellwoo.kinoguru.shared.movie.BaseUrls
 import com.shellwoo.kinoguru.shared.movie.ui.RatingFormatter
-import kotlinx.android.synthetic.main.movie_search_success_item.view.*
 import com.shellwoo.kinoguru.design.resource.R as designR
 
 class MovieSearchSuccessItemViewHolder(
     private val ratingFormatter: RatingFormatter,
     parent: ViewGroup,
 ) : RecyclerView.ViewHolder(parent.inflate(R.layout.movie_search_success_item, false)) {
+
+    private val binding by viewBinding(MovieSearchSuccessItemBinding::bind)
 
     private val requestManager: RequestManager by lazy { Glide.with(itemView) }
 
@@ -47,10 +50,10 @@ class MovieSearchSuccessItemViewHolder(
         val movie = item.value
         renderPoster(movie.posterPath)
         renderRating(movie.voteAverage)
-        with(itemView) {
+        with(binding) {
             title.text = movie.title
             description.text = movie.overview
-            setOnClickListener { onClickListener(item) }
+            root.setOnClickListener { onClickListener(item) }
         }
     }
 
@@ -64,28 +67,28 @@ class MovieSearchSuccessItemViewHolder(
                 .placeholder(android.R.drawable.ic_menu_camera)
                 .listener(posterRequestListener)
                 .error(android.R.drawable.ic_menu_camera)
-                .into(itemView.poster)
+                .into(binding.poster)
         } else {
             renderPosterLoaded()
 
-            itemView.poster.setImageResource(android.R.drawable.ic_menu_camera)
+            binding.poster.setImageResource(android.R.drawable.ic_menu_camera)
         }
     }
 
     private fun renderPosterLoading() {
-        itemView.poster.isInvisible = true
-        itemView.posterSkeleton.isVisible = true
+        binding.poster.isInvisible = true
+        binding.posterSkeleton.isVisible = true
     }
 
     private fun renderPosterLoaded() {
-        itemView.poster.isInvisible = false
-        itemView.posterSkeleton.isVisible = false
+        binding.poster.isInvisible = false
+        binding.posterSkeleton.isVisible = false
     }
 
     private fun renderRating(value: Double?) {
-        with(itemView) {
-            rating.setBackgroundColor(getRatingBackgroundColor(value))
-            rating.text = ratingFormatter.format(value)
+        with(binding.rating) {
+            setBackgroundColor(getRatingBackgroundColor(value))
+            text = ratingFormatter.format(value)
         }
     }
 
