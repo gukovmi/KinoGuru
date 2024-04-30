7571569ba45324fd80602a8de6016f8cd52ce640
gukovmi <gukovmi96@yandex.ru>
2024-05-01 00:27:48 +0700

    Migrate profile screen on VB

--- feature/profile/src/main/java/com/shellwoo/kinoguru/feature/profile/ui/ProfileFragment.kt
+++ feature/profile/src/main/java/com/shellwoo/kinoguru/feature/profile/ui/ProfileFragment.kt
@@ -9,11 +9,13 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.net.toUri
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestManager
 import com.shellwoo.kinoguru.core.ui.component.BaseFragment
 import com.shellwoo.kinoguru.core.ui.ext.setResultListener
 import com.shellwoo.kinoguru.feature.profile.R
+import com.shellwoo.kinoguru.feature.profile.databinding.ProfileFragmentBinding
 import com.shellwoo.kinoguru.feature.profile.di.ProfileComponentViewModel
 import com.shellwoo.kinoguru.feature.profile.presentation.ProfileState
 import com.shellwoo.kinoguru.feature.profile.presentation.ProfileViewModel
@@ -21,7 +23,6 @@ import com.shellwoo.kinoguru.shared.language.ui.LanguageNameConverter
 import com.shellwoo.kinoguru.shared.language.ui.LanguageResultContract
 import com.shellwoo.kinoguru.shared.theme.ui.ThemeNameConverter
 import com.shellwoo.kinoguru.shared.theme.ui.ThemeResultContract
-import kotlinx.android.synthetic.main.profile_fragment.*
 import javax.inject.Inject
 
 class ProfileFragment : BaseFragment(R.layout.profile_fragment) {
@@ -29,6 +30,7 @@ class ProfileFragment : BaseFragment(R.layout.profile_fragment) {
     private val componentViewModel: ProfileComponentViewModel by viewModels()
 
     private val viewModel: ProfileViewModel by viewModels(factoryProducer = ::viewModelFactory)
+    private val binding by viewBinding(ProfileFragmentBinding::bind)
 
     private val requestManager: RequestManager by lazy { Glide.with(this) }
 
@@ -63,9 +65,11 @@ class ProfileFragment : BaseFragment(R.layout.profile_fragment) {
     private fun initListeners() {
         setResultListener(LanguageResultContract, viewModel::selectLanguage)
         setResultListener(ThemeResultContract, viewModel::selectTheme)
-        language.setOnClickListener { viewModel.openLanguageScreen() }
-        photo.setOnClickListener { pickPhotoLauncher.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)) }
-        theme.setOnClickListener { viewModel.openThemeScreen() }
+        with(binding) {
+            language.setOnClickListener { viewModel.openLanguageScreen() }
+            photo.setOnClickListener { pickPhotoLauncher.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)) }
+            theme.setOnClickListener { viewModel.openThemeScreen() }
+        }
     }
 
     private fun renderState(state: ProfileState) {
@@ -77,31 +81,37 @@ class ProfileFragment : BaseFragment(R.layout.profile_fragment) {
     }
 
     private fun renderInitialState() {
-        toolbar.isVisible = false
-        content.isVisible = false
-        progressBar.isVisible = false
+        with(binding) {
+            toolbar.isVisible = false
+            content.isVisible = false
+            progressBar.isVisible = false
+        }
     }
 
     private fun renderLoadingState() {
-        toolbar.isVisible = true
-        content.isVisible = false
-        progressBar.isVisible = true
+        with(binding) {
+            toolbar.isVisible = true
+            content.isVisible = false
+            progressBar.isVisible = true
+        }
     }
 
     private fun renderContentState(state: ProfileState.Content) {
-        toolbar.isVisible = true
-        content.isVisible = true
-        progressBar.isVisible = false
+        with(binding) {
+            toolbar.isVisible = true
+            content.isVisible = true
+            progressBar.isVisible = false
 
-        state.name?.let { name.setText(it) }
-        state.email?.let { email.setText(it) }
-        state.photoUrl?.toUri()?.let { photoUri ->
-            requestManager.load(photoUri)
-                .error(R.drawable.person)
-                .into(photo)
-        } ?: photo.setImageResource(R.drawable.person)
+            state.name?.let { name.setText(it) }
+            state.email?.let { email.setText(it) }
+            state.photoUrl?.toUri()?.let { photoUri ->
+                requestManager.load(photoUri)
+                    .error(R.drawable.person)
+                    .into(photo)
+            } ?: photo.setImageResource(R.drawable.person)
 
-        language.setText(languageNameConverter.toName(state.language))
-        theme.setText(themeNameConverter.toName(state.theme))
+            language.setText(languageNameConverter.toName(state.language))
+            theme.setText(themeNameConverter.toName(state.theme))
+        }
     }
 }
