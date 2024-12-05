a2b74029d94ac6fca90cbc0f5893a085c50e35af
gukovmi <gukovmi96@yandex.ru>
2024-12-05 18:35:38 +0700

    Migrate theme screen on Compose

--- feature/theme/src/main/kotlin/com/shellwoo/kinoguru/feature/theme/ui/ThemeFragment.kt
+++ feature/theme/src/main/kotlin/com/shellwoo/kinoguru/feature/theme/ui/ThemeFragment.kt
@@ -2,48 +2,50 @@ package com.shellwoo.kinoguru.feature.theme.ui
 
 import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.compose.runtime.getValue
 import androidx.fragment.app.viewModels
-import by.kirich1409.viewbindingdelegate.viewBinding
+import com.shellwoo.kinoguru.core.presentation.observeAsNotNullableState
 import com.shellwoo.kinoguru.core.ui.component.BaseFragment
+import com.shellwoo.kinoguru.core.ui.component.NO_CONTENT_LAYOUT_ID
+import com.shellwoo.kinoguru.core.ui.ext.createComposeView
 import com.shellwoo.kinoguru.core.ui.ext.setResult
-import com.shellwoo.kinoguru.feature.theme.R
-import com.shellwoo.kinoguru.feature.theme.databinding.ThemeFragmentBinding
 import com.shellwoo.kinoguru.feature.theme.di.ThemeComponentViewModel
 import com.shellwoo.kinoguru.feature.theme.presentation.ThemeViewModel
 import com.shellwoo.kinoguru.shared.theme.domain.entity.Theme
+import com.shellwoo.kinoguru.shared.theme.ui.ThemeNameConverter
 import com.shellwoo.kinoguru.shared.theme.ui.ThemeResultContract
 import javax.inject.Inject
 
-class ThemeFragment : BaseFragment(R.layout.theme_fragment) {
+class ThemeFragment : BaseFragment(NO_CONTENT_LAYOUT_ID) {
 
     private val componentViewModel: ThemeComponentViewModel by viewModels()
     private val viewModel: ThemeViewModel by viewModels(factoryProducer = { viewModelFactory })
-    private val binding by viewBinding(ThemeFragmentBinding::bind)
 
     @Inject
-    lateinit var themeAdapter: ThemeAdapter
+    lateinit var themeNameConverter: ThemeNameConverter
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
         componentViewModel.component.inject(this)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
+        createComposeView {
+            val themes by viewModel.themes.observeAsNotNullableState()
 
-        binding.toolbar.setNavigationOnClickListener { viewModel.close() }
-        themeAdapter.setOnClickListener(::selectTheme)
-        viewModel.themes.observe(viewLifecycleOwner, ::renderThemes)
-    }
+            ThemeScreen(
+                themes = themes,
+                themeNameConverter = themeNameConverter::toName,
+                onThemeClick = ::selectTheme,
+                onNavigationIconClick = viewModel::close,
+            )
+        }
 
     private fun selectTheme(theme: Theme) {
         setResult(ThemeResultContract, theme)
         viewModel.close()
-    }
-
-    private fun renderThemes(values: List<Theme>) {
-        themeAdapter.submitList(values)
-        binding.themes.adapter = themeAdapter
     }
 }
